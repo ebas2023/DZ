373d24b221c22af5da39cd2df9623e6bdacedb21
Vasya Pupkin <vasya.pupkin@email.com>
2023-07-24 22:34:21 +0300

    добавил Д4

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
-﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-2, 3, 7 -> 7
-44 5 78 -> 78
-22 3 9 -> 22*/
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
 
-Console.WriteLine("Введите число a:");
-int first = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите число b:");
-int second = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите число c:");
-int third = int.Parse(Console.ReadLine()!);
-int max=first;
-if (second>max) max=second;
-if (third>max) max=third;
-Console.WriteLine ($"Max={max}");
+Console.Write("Введите число N:");
+int n = int.Parse(Console.ReadLine()!);
+int num=1;
+while(num<=n)
+{
+    if (num%2==0) 
+    {
+        Console.Write ($"{num} ");
+    }
+num ++;
+}
