a74a5b5243fdaef732dc6ca46ce7cd07bfafc060
Vasya Pupkin <vasya.pupkin@email.com>
2023-07-24 22:05:42 +0300

    добавил ДЗ3

--- Program.cs
+++ Program.cs
@@ -1,32 +1,16 @@
-﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-a = 5; b = 7 -> max = 7
-a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3*/
+﻿
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
 
 Console.WriteLine("Введите число a:");
 int first = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите число b:");
-int second = int.Parse(Console.ReadLine()!);
-if (first > second)
+if (first %2==0)
 {
-    Console.WriteLine($"max={first}");
+    Console .WriteLine ($"Число {first} четное");
 }
 else
 {
-    Console.WriteLine($"max={second}");
+    Console .WriteLine ($"Число {first} не четное");
 }
-/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-2, 3, 7 -> 7
-44 5 78 -> 78
-22 3 9 -> 22*/
-
-Console.WriteLine("Введите число a:");
-int first = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите число b:");
-int second = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите число c:");
-int third = int.Parse(Console.ReadLine()!);
-int max=first;
-if (second>max) max=second;
-if (third>max) max=third;
-Console.WriteLine ($"Max={max}");
